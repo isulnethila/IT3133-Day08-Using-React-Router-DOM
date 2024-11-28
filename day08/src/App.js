5ea3ec8ecd21481de56d5ff700e64b75d1e5ee7f
isulnethila <139636196+isulnethila@users.noreply.github.com>
2024-11-28 15:14:02 +0530

    create  navigation list and link pages using router

--- day08/src/App.js
+++ day08/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link,NavLink } from 'react-router-dom';
 import About from './Components/About';
 import ContactUs from './Components/ContactUs';
 import Dashbord from './Components/Dashbord';
@@ -9,6 +9,21 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Router>
+      <nav>
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+
+        <Routes>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contact' element={<ContactUs/>}/>
+        </Routes>
+      </Router>
       
     </div>
   );
