a0f9690a343216c6cdec8e685f63c6d67ddf212e
isulnethila <139636196+isulnethila@users.noreply.github.com>
2024-11-28 14:50:10 +0530

    import componets in to app.js

--- day08/src/App.js
+++ day08/src/App.js
@@ -1,4 +1,9 @@
-
+import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
+import About from './Components/About';
+import ContactUs from './Components/ContactUs';
+import Dashbord from './Components/Dashbord';
+import Home from './Components/Home';
+import Login from './Components/Login';
 import './App.css';
 
 function App() {
--- day08/src/Components/ContactUs.js
+++ day08/src/Components/ContactUs.js
@@ -1,5 +1,5 @@
 export default function ContactUs(){
     return(
-        <div></div>
+        <div>Welcome to Contact us page</div>
     );
 }
