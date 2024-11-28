5abb25ed251a603f6f6f10b9a77001c63f5bb3f9
isulnethila <139636196+isulnethila@users.noreply.github.com>
2024-11-28 16:19:05 +0530

    update login.js

--- day08/src/Components/Login.js
+++ day08/src/Components/Login.js
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 export default function Login(){
+    const[user,setUser]=useState({username:"",password:"",error:""})
+    const navigate=useNavigate();
+    const getTextInput=(e)=>{
+        const{name,value}=e.target;
+        setUser((prevUser)=>({
+            ...prevUser,[name]:value,
+        }))
+
+
+    }
+    const aunthenticate=()=>{
+        if(user.username === "Admin" && user.pwd ==="abc@123"){
+            setUser({username: "",pwd:"",error:""})
+            navigate('/dash');
+        }else{
+            setUser({'error':'Please check your username and password !'})
+        }
+    }
     return(
-        <div></div>
+        <div>
+            <h1>Login here</h1>
+            <table>
+                <tr>
+                    <td>Username:</td>
+                    <td><input type="text" name="username" onChange={getTextInput} /></td>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                    <td><input type="password" name="pwd" onChange={getTextInput} /></td>
+                </tr>
+                <tr>
+                    <td style={{ color: 'red' }} colSpan={2}>{user.error}</td>
+                </tr>
+            </table>
+            <button onClick={aunthenticate}>Login</button>
+
+
+        </div>
     );
 }
